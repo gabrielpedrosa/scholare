7905a416cb1cbcd78652147376410f82a6934aa4
gabrielpedrosa <gabriel.pedrosaf@gmail.com>
2018-10-15 01:41:48 -0300

    Criando Index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+//
+?>
+<!Doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+    <title>Pestalozzi</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+      <img class="logo" src="img/logo.jpg" alt="logo" width="50" height="50">
+        <nav>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+
+  </body>
+</html>
