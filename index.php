8589f4d8c88a7ea57498a5f2dc825ed0f9283064
gabriel.pedrosa <gabriel.pedrosaf@gmail.com>
2018-10-15 18:18:04 -0400

    Criando Nav Bar Responsiva

--- index.php
+++ index.php
@@ -3,25 +3,31 @@
 ?>
 <!Doctype html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/all.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <title>Pestalozzi</title>
   </head>
   <body>
     <header>
-      <div class="container">
-      <img class="logo" src="img/logo.jpg" alt="logo" width="50" height="50">
-        <nav>
-          <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="index.php">Home</a></li>
-          </ul>
-        </nav>
-      </div>
+      <img id="logo"src="img/logo.jpg">
+      <div id="logo"> LOGO </div>
+      <nav>
+        <ul>
+          <li><a href="#" class="active">Home</a></li>
+          <li><a href="#">Cadastros</a></li>
+          <li><a href="#">Notas</a></li>
+          <li><a href="#">Presença</a></li>
+          <li><a href="#">Sobre</a></li>
+        </ul>
+      </nav>
+      <div class="menu-toggle"><i class="fa fa-bars" aria-hidden="true"></i></div>
+
     </header>
 
-
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="js/all.js"></script>
   </body>
 </html>
---
+++ index1.php
@@ -0,0 +1,42 @@
+<?php
+//
+?>
+<!Doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+    <title>Pestalozzi</title>
+  </head>
+  <body>
+    <div id="menu">
+      <a href="#" class="brand"><img src="img/logo.jpg"></a>
+      <p>Scholare</p>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Cadastros</a>
+            <ul>
+              <li><a href="#">Aluno</a></li>
+              <li><a href="#">Professor</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Notas</a>
+            <ul>
+              <li><a href="#">Cadastrar Notas</a></li>
+              <li><a href="#">Exibir Notas</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Presença</a>
+            <ul>
+              <li><a href="#">Cadastrar Presença</a></li>
+              <li><a href="#">Exibir Presença</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Sobre</a></li>
+        </ul>
+      </nav>
+    </div>
+    <section></section>
+  </body>
+</html>
