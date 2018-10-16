4d10876941ad2b80831c1857421b5766ddb64a97
gabriel.pedrosa <gabriel.pedrosaf@gmail.com>
2018-10-15 23:54:00 -0400

    Criando Slider pt1

--- index.php
+++ index.php
@@ -11,36 +11,58 @@
     <title>Scholare</title>
   </head>
   <body>
+    <div id="loader"class="carregar"></div>
     <header>
       <img id="logo"src="img/logo.jpg">
       <div id="logo"> Scholare </div>
       <nav>
         <ul>
           <li><a href="#">Home</a></li>
-          <li class="submenu"><a href="#">Cadastros</a>
+          <li class="submenu hide"><a href="#">Cadastros</a>
             <ul>
               <li><a href="#">Aluno</a></li>
               <li><a href="#">Professor</a></li>
             </ul>
           </li>
-          <li class="submenu"><a href="#">Notas</a>
+          <li class="submenu hide"><a href="#">Notas</a>
             <ul>
               <li><a href="#">Cadastrar Notas</a></li>
               <li><a href="#">Exibir Notas</a></li>
             </ul>
           </li>
-          <li class="submenu"><a href="#">Presença</a>
+          <li class="submenu hide"><a href="#">Presença</a>
             <ul>
               <li><a href="#">Cadastrar Presença</a></li>
               <li><a href="#">Exibir Presença</a></li>
             </ul>
           </li>
           <li><a href="#">Sobre</a></li>
+          <li><a href="#">Cadastre-se</a></li>
+          <li><a href="#">Login</a></li>
         </ul>
       </nav>
       <div class="menu-toggle"><i class="fa fa-bars" aria-hidden="true"></i></div>
-
     </header>
+
+    <div class="container">
+      <div id="carousel">
+        <div id="itens">
+          <ul>
+            <li><div class="item"><img class="img" src="img/pestalozzi01.jpg"></div></li>
+            <li><div class="item"><img class="img" src="img/pestalozzi02.jpg"></div></li>
+            <li><div class="item"><img class="img" src="img/pestalozzi03.jpg"></div></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div>
+        <h6>Desenvolvido por estudantes do Instituto Federal de Educação, Ciência e Tecnologia de Rondônia
+          <br>
+          © 2018 Bootstrap.</h6>
+        </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script type="text/javascript" src="js/all.js"></script>
--- js/all.js
+++ js/all.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  $( window ).on( "load", function() {
+    $("#loader").delay(1000).fadeOut("slow");
+    });
   $('.menu-toggle').click(function(){
     $('nav').toggleClass('active');
-  })
+  });
   $('ul li').click(function() {
     $(this).siblings().removeClass('active');
     $(this).toggleClass('active');
